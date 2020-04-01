1d340892f5f7d0be92be91382d28169f096a72c9
MiriamLeis <42590067+MiriamLeis@users.noreply.github.com>
2020-04-01 13:02:55 +0200

    Monedas y contador de monedas (#3)
    * creado collectableEC
    
    * moneda creada
    
    funciona
    
    * contador de monedas

---
+++ src/CollectableEC.cpp
@@ -0,0 +1,54 @@
+#include "CollectableEC.h"
+#include "ComponentsManager.h"
+#include "Entity.h"
+#include "FactoriesFactory.h"
+#include "RigidbodyPC.h"
+#include "CoinCounterC.h"
+#include "Scene.h"
+
+#include <json.h>
+
+void CollectableEC::destroy() {
+    setActive(false);
+    scene->getComponentsManager()->eraseEC(this);
+}
+
+void CollectableEC::checkEvent() {
+    if (reinterpret_cast<RigidbodyPC*>(father->getComponent("RigidbodyPC"))
+            ->collidesWith("Player")) {
+        reinterpret_cast<CoinCounterC*>(scene->getEntitybyId("GameManager")->getComponent("CoinCounterC"))->pickCoin();
+        scene->deleteEntity(father);
+    }
+}
+
+int CollectableEC::getValue()
+{
+    return value_;
+}
+
+void CollectableEC::setValue(int value)
+{
+    value_ = value;
+}
+
+// FACTORY INFRASTRUCTURE
+CollectableECFactory::CollectableECFactory() = default;
+
+Component* CollectableECFactory::create(Entity* _father, Json::Value& _data,
+                                  Scene* _scene) {
+    CollectableEC* collectable = new CollectableEC();
+    _scene->getComponentsManager()->addEC(collectable);
+
+    collectable->setFather(_father);
+    collectable->setScene(_scene);
+
+    if (!_data["value"].asInt())
+        throw std::exception("CollectableEC: value is not an int");
+    collectable->setValue(_data["value"].asInt());
+
+    collectable->setActive(true);
+
+    return collectable;
+};
+
+DEFINE_FACTORY(CollectableEC);
---
+++ src/CollectableEC.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "EventComponent.h"
+#include "Factory.h"
+
+DECLARE_FACTORY(CollectableEC);
+
+class CollectableEC : public EventComponent {
+  private:
+      // value of the collectable
+      int value_;
+
+  public:
+      CollectableEC() = default;
+    ~CollectableEC() = default;
+
+    virtual void destroy();
+
+    virtual void checkEvent();
+
+    // get the value of the collectable
+    int getValue();
+    // set the value of the collectable
+    void setValue(int value);
+};
