b9306853e0e36410d0d4c9b556fcd58e19e70fbf
Alejandro Placer <aplazer@ucm.es>
2020-04-29 21:23:58 +0200

    chore: rework gshift level

--- src/gshift.cpp
+++ src/gshift.cpp
@@ -6,14 +6,14 @@
 int main(int argc, char* args[])
 #else
 #include <windows.h>
-int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE prevInstance, LPSTR lmCmdLine,
+int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE prevInstance, LPSTR lmCmdLine, 
                    int nCmdShow)
 #endif
 {
     _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
     try {
         Game game;
-        if (game.init("Game"))
+        if (game.init("GSHIFT"))
             game.run();
     } catch (std::exception& e) {
         std::cout << "\n"
